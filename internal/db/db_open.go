fa693448f5c138d2eac9e56918f5feba24995102
Gera Anggara Putra <gera@plabs.id>
2025-07-17 08:22:32 +0700

    refactor/template-enhancements: delete ping context when open sql

--- internal/db/db_open.go
+++ internal/db/db_open.go
@@ -1,7 +1,6 @@
 package db
 
 import (
-	"context"
 	"database/sql"
 	"time"
 
@@ -17,14 +16,6 @@ func openSQL(driver, source string, opt *config.Config) (db *sql.DB, err error) 
 	db.SetMaxOpenConns(opt.DB.MaxOpenConn)
 	db.SetMaxIdleConns(opt.DB.MaxIdleConn)
 	db.SetConnMaxIdleTime(time.Duration(opt.DB.MaxConnIdleTime))
-
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-	defer cancel()
-
-	err = db.PingContext(ctx)
-	if err != nil {
-		return
-	}
 
 	return
 }
